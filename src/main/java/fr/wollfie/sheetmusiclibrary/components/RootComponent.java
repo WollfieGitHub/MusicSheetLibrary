690fc059e0656706c1dd0876ea8e7e64c23c5581
wollfie <leowolllff@gmail.com>
2022-10-27 20:34:36 +0200

    Modify default theme's accent color

--- src/main/java/fr/wollfie/sheetmusiclibrary/components/RootComponent.java
+++ src/main/java/fr/wollfie/sheetmusiclibrary/components/RootComponent.java
@@ -17,17 +17,18 @@ public class RootComponent extends BorderPane {
         TopToolbar topToolbar = new TopToolbar(stage);
         setTop(topToolbar);
         
+        String borderStyle = "-fx-border-width: 1;" +
+                "-fx-border-radius: 10 10 15 15;" +
+                "-fx-border-color: " + "grey" + ";";
+        
         String backgroundStyle = ("-fx-background-color: linear-gradient(to top," +
                 ThemeManager.hexColorFrom(Theme.Category.Background, Theme.Shade.Dark2) + " 80%, " +
-                ThemeManager.hexColorFrom(Theme.Category.Primary, Theme.Shade.Light1) + ");" +
-                "-fx-border-width: 1;" +
-                "-fx-border-radius: 10 10 15 15;" +
-                "-fx-border-color: " + "grey" + ";");
+                ThemeManager.hexColorFrom(Theme.Category.Primary, Theme.Shade.Light1) + ");");
 
-        setStyle(backgroundStyle + "-fx-background-radius: 15");
+        setStyle(backgroundStyle + borderStyle + "-fx-background-radius: 15");
 
         topToolbar.stageMaximizedProperty().addListener((o, oldB, newB) -> {
-            setStyle(backgroundStyle + (newB ? "-fx-background-radius: 0;" : "-fx-background-radius: 15;"));
+            setStyle(backgroundStyle + (newB ? "-fx-background-radius: 0;" : borderStyle + "-fx-background-radius: 15;"));
         });
     }
 }
