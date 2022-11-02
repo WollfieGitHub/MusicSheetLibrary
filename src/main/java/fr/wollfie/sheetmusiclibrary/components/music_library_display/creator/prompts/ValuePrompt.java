9692d1195252f760d5c244022d881bf08202c7a3
wollfie <leowolllff@gmail.com>
2022-11-02 11:15:10 +0100

    Format creator

--- src/main/java/fr/wollfie/sheetmusiclibrary/components/music_library_display/creator/prompts/ValuePrompt.java
+++ src/main/java/fr/wollfie/sheetmusiclibrary/components/music_library_display/creator/prompts/ValuePrompt.java
@@ -1,6 +1,8 @@
 package fr.wollfie.sheetmusiclibrary.components.music_library_display.creator.prompts;
 
+import fr.wollfie.sheetmusiclibrary.controllers.ThemedLabel;
 import fr.wollfie.sheetmusiclibrary.theme.ThemeManager;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -9,24 +11,27 @@ import java.util.function.Consumer;
 
 public abstract class ValuePrompt<T> extends VBox {
     
-    protected final int fontSize = 10;
-    
-    // language=CSS-FX
-    protected final String baseStyle = "-fx-font-size: " + fontSize + ";" +
-            "-fx-font-fill: " + ThemeManager.getTextColorHexFrom(null) + ";";
-    
     protected final Consumer<T> callback;
     protected final String prompt;
-    
+
+    /**
+     * A component to prompt for a value, depending on the type T
+     * @param prompt The text to display to prompt to explain what should the input be
+     * @param callback The callback function when the result has been retrieved
+     */
     public ValuePrompt(String prompt, Consumer<T> callback) {
         this.prompt = prompt;
         this.callback = callback;
+        
+        setAlignment(Pos.CENTER);
 
-        Label promptLbl = new Label(prompt);
+        ThemedLabel promptLbl = new ThemedLabel(prompt, 20);
         getChildren().addAll(promptLbl, getNode());
     }
-    
+
+    /** @return the node to mount on the creator component and prompt for a value */
     protected abstract Node getNode();
 
+    /** Request the focus for the value prompt */
     public abstract void getFocus();
 }
